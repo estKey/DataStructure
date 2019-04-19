75fb50c7d043f4953bb797cd76377d3a9f2b6f35
estKey <estkey@outlook.com>
2019-04-18 21:04:48 -0400

    Refactor

---
+++ C++/DataStructure/DataStructure/format.sh
@@ -0,0 +1,24 @@
+﻿#!/bin/bash
+# Author: estKey
+
+echo "Processing..."
+for i in *.cpp *.hpp
+ do
+   dist_folder=${i%.*}
+   # echo $dist_folder
+   # skip the main.cpp and stdafx.hpp for creating folders
+   if [ "$dist_folder" = "main" -o "$dist_folder" = "stdafx" ]
+   then
+        echo "skip $dist_folder"
+        continue
+   fi
+   if [ ! -d $dist_folder ]
+   then
+   mkdir ${dist_folder}
+   echo "the folder with the name of $dist_folder has been created."
+   fi
+   mv $i ./${dist_folder}/$i
+   echo "$i has been moved to the same name folder of $dist_folder"
+done
+echo "Done."
+read -n 1 -p "Press any key to terminate"
---
+++ C++/DataStructure/format.sh
@@ -0,0 +1,18 @@
+﻿
+#!/bin/bash
+
+# 同名文件与文件夹的意思是，文件名中的下划线_前面的部分相同
+# 批量自动把同名文件分别放入同名文件夹中
+
+# 处理的文件是后缀名为.jpg的文件
+for i in *.cpp *.hpp;do
+    destination_folder=${i%.*}
+   echo $destination_folder
+    if [ destination_folder -eq "main" -o destination_folder -eq "stdafx"] then continue
+    if [ ! -d $destination_folder ];then
+        mkdir ${destination_folder}
+        echo "the folder with the name of $destination_folder has been created."
+    fi
+    mv $i ./${destination_folder}/$i
+    echo "$i has been moved to the same name folder of $destination_folder"
+done
