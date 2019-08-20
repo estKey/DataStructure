cf3aaa81d7891dfd1abc0db3f08366d3db9d4cb2
estKey <estkey@outlook.com>
2019-08-20 01:57:03 -0400

    recovery CPP

---
+++ C++/DataStructure/DataStructure.cpp
@@ -0,0 +1,12 @@
+﻿// DataStructure.cpp : Defines the entry point for the application.
+//
+
+#include "DataStructure.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Data Structure" << endl;
+	return 0;
+}
---
+++ C++/DataStructure/Stack.hpp
@@ -0,0 +1,6 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <string>
+#include <stdexcept>
